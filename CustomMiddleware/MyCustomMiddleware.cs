becd7fe1ce455f721ee3205d5e68a0d068c97c39
bibekpandey0521 <bibekpandey276@gmail.com>
2025-07-21 11:42:07 +0545

    Middleware custom starting only

---
+++ CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MiddlewareExample.CustomMiddleware
+{
+    public class MyCustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("My Custom Middleware - Ends\n");
+            await next(context);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
+using MiddlewareExample.CustomMiddleware;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddleware>();
 var app = builder.Build();
 
 
-//app.Run method is middleware and it is nature of app.Run() method that it doesnot forward the request to subsequent middleware
-//app.Run(async (HttpContext context) => {
-//    await context.Response.WriteAsync("Hello");
-//});
 
 //middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) => { 
@@ -15,11 +14,7 @@ app.Use(async (HttpContext context, RequestDelegate next) => {
 
 //app.MapGet("/", () => "Hello World!");
 //middleware 2 
-app.Use(async (HttpContext context,RequestDelegate next) => {
-    await context.Response.WriteAsync("hello again middleware 2 \n");
-    await next(context);
-
-});
+app.UseMiddleware<MyCustomMiddleware>();
 
 
 app.Run(async (HttpContext context) => {
